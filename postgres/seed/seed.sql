2220e2d5fce6fc9942b8a09cd750867f832a523a
Igor Houwat <58915539+ihouwat@users.noreply.github.com>
2020-07-03 23:11:08 -0400

    start adding signin authentication

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -2,8 +2,8 @@
 
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
+INSERT into users (name, email, entries, joined) values ('a', 'a@gmail.com', 5, '2018-01-01');
 -- password is 'a'
-INSERT into login (hash, email) values ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'jessie@gmail.com');
+INSERT into login (hash, email) values ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'a@gmail.com');
 
 COMMIT;
