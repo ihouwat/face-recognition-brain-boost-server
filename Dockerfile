1cb9e9cf997d80009af122b438c321517ce7cd84
Igor Houwat <58915539+ihouwat@users.noreply.github.com>
2020-07-04 15:47:08 -0400

    add redis, JWTs, auth middleware

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM node:erbium
 
+# Create app directory
 WORKDIR /usr/src/face-recognition-brain-boost-server
 
+#Install app dependencies
 COPY ./ ./
-
 RUN npm install
 
 CMD ["/bin/bash"]
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 
 const db = knex({
   client: 'pg',
@@ -32,9 +33,9 @@ app.use(cors());
 app.get('/', (req, res) => {res.send(db.users, 'app is working')})
 app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister (req, res, db, bcrypt)})
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
-app.post('/profile/:id', (req, res) => {profile.handleProfileUpdate(req, res, db)})
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => {profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => {profile.handleProfileUpdate(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 
